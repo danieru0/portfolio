1d182644ad9e2ce84e0603411b236f1ac6560d96
unknown <dabrowskidaniel006@gmail.com>
2020-11-28 23:29:17 +0100

    Updates

---
+++ precache-manifest.ddb572e158bdc1dc303b25165c170657.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d03862a9ad49e22751213b676eaa9b8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "229736ac0e4960d927d2",
+    "url": "/portfolio/static/css/2.32144bb6.chunk.css"
+  },
+  {
+    "revision": "6e68b994f951111e668f",
+    "url": "/portfolio/static/css/main.aa35d09a.chunk.css"
+  },
+  {
+    "revision": "229736ac0e4960d927d2",
+    "url": "/portfolio/static/js/2.6a3b3fc8.chunk.js"
+  },
+  {
+    "revision": "3a0622fd24dedd0f8971cf19b181e550",
+    "url": "/portfolio/static/js/2.6a3b3fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e68b994f951111e668f",
+    "url": "/portfolio/static/js/main.7cd109a9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "84423a6d524761c11bcbced8789a35cf",
+    "url": "/portfolio/static/media/chat-v2.84423a6d.png"
+  },
+  {
+    "revision": "850e888b97568b7fc72c52f342bfde65",
+    "url": "/portfolio/static/media/global.850e888b.png"
+  },
+  {
+    "revision": "27f1b5ad0d35e23f2c886de3e7c34014",
+    "url": "/portfolio/static/media/korean-practice.27f1b5ad.png"
+  },
+  {
+    "revision": "12f003923d41c814bbccc970eb32bd2a",
+    "url": "/portfolio/static/media/notes.12f00392.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/portfolio/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/portfolio/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/portfolio/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/portfolio/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "6d1da070248ed5f5def45b07dad061a3",
+    "url": "/portfolio/static/media/video-player.6d1da070.png"
+  },
+  {
+    "revision": "5d6930cff90b7ed01c5c775e40ea3856",
+    "url": "/portfolio/static/media/video-v2.5d6930cf.png"
+  },
+  {
+    "revision": "027b066d877ec650372080718000fd93",
+    "url": "/portfolio/static/media/windowsapp.027b066d.jpg"
+  }
+]);
